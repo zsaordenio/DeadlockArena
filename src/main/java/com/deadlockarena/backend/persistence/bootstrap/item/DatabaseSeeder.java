01ed6d1d024788977f404408fb59c8c0b45bb238
Eric Opoku <eric.opoku20@yahoo.com>
2019-09-21 16:13:28 -0400

    Removing space

--- src/main/java/com/deadlockarena/backend/persistence/bootstrap/item/DatabaseSeeder.java
+++ src/main/java/com/deadlockarena/backend/persistence/bootstrap/item/DatabaseSeeder.java
@@ -17,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
-
 	private final PotionInventoryRepository potionInventoryRepository;
 
 	@Autowired
